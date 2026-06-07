5b76102b29332e99e2536111ca6684817cc5b50c
Rishi Kumar <159180701+Ghostoperators@users.noreply.github.com>
2026-06-07 17:57:16 +0530

    Initial commit - Django Online Exam project

--- onlineExam/settings.py
+++ onlineExam/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'onlineExam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'online_exam',
-        'USER': 'root',
-        'PASSWORD': 'Hari@12345',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
